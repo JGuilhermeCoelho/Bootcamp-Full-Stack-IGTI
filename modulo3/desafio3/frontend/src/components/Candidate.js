b93b5ea279bad389ea03a831ffe781c0be7b7ed7
Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2020-08-23 18:05:53 -0300

    adding desafio3 project

---
+++ modulo3/desafio3/frontend/src/components/Candidate.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Position from "./Position";
+import Picture from "./Picture";
+
+export default function Candidate({ candidate, position }) {
+  const { id, name, votes } = candidate;
+  const imageSource = `${id}.jpg`;
+
+  return (
+    <div>
+      <Position> {position} </Position>
+      <Picture imageSource={imageSource} description={name} title={name} />
+      {name} - {votes}
+    </div>
+  );
+}
